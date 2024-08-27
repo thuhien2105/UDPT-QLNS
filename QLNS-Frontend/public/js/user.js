e2591c59eac477ff41c139cd8f4f003dfb83b6ba
dthgiang <duongthanhgiang.0108@gmail.com>
2024-08-27 17:31:16 +0700

    Update avatar default and name of user in header (missing role in API)

--- QLNS-Frontend/public/js/user.js
+++ QLNS-Frontend/public/js/user.js
@@ -45,5 +45,35 @@ $(document).ready(function () {
             $("#phone_number_0").val("");
         }
     }
+
+    function populateUserName() {
+        var token = Cookies.get("token");
+        var id = Cookies.get("id");
+
+        if (id) {
+            $.ajax({
+                url: `http://127.0.0.1:8000/api/employees/${id}`,
+                method: "GET",
+                dataType: "json",
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                },
+                success: function (data) {
+                    // Lấy tên đầy đủ từ dữ liệu API
+                    var fullName = data.employee.name || "";
+
+                    // Điền tên đầy đủ vào phần tử HTML tương ứng
+                    $("#user_name").text(fullName);
+                },
+                error: function () {
+                    $("#activity-details").html(
+                        "<p>An error occurred while fetching employee details.</p>"
+                    );
+                },
+            });
+        }
+    }
+
     populateFormFromCookies();
+    populateUserName();
 });
--- QLNS-Frontend/resources/views/page/header.blade.php
+++ QLNS-Frontend/resources/views/page/header.blade.php
@@ -17,18 +17,30 @@
     <div></div>
     <div class="o-dropdown dropdown o_user_menu d-none d-md-block pe-0 o-dropdown--no-caret">
         <button type="button" class="dropdown-toggle py-1 py-lg-0" tabindex="0" aria-expanded="false">
-            <img class="o_avatar o_user_avatar rounded" alt="User"
-                src="http://localhost:8069/web/image?model=res.users&amp;field=avatar_128&amp;id=2" />
+            <img class="o_avatar o_user_avatar rounded" alt="User" src="/image/cat.png" />
             <small class="oe_topbar_name d-none ms-2 text-start smaller lh-1 text-truncate d-lg-inline-block"
-                style="max-width: 200px">Mitchell Admin</small>
+                style="max-width: 200px">
+                <span id="user_name"></span> </span><span id="role">Admin</span>
+            </small>
         </button>
         <!-- Dropdown menu that will be toggled -->
         <div role="menu" class="o-dropdown--menu dropdown-menu" style="display: none;">
-            <a class="dropdown-item focus" role="menuitem" tabindex="0" href="https://www.odoo.com/documentation/17.0" data-menu="documentation">Documentation</a>
-            <a class="dropdown-item" role="menuitem" tabindex="0" href="https://www.odoo.com/help" data-menu="support">Support</a>
+            <a class="dropdown-item focus" role="menuitem" tabindex="0" href="https://www.odoo.com/documentation/17.0"
+                data-menu="documentation">Documentation</a>
+            <a class="dropdown-item" role="menuitem" tabindex="0" href="https://www.odoo.com/help"
+                data-menu="support">Support</a>
             <div role="separator" class="dropdown-divider"></div>
             <span class="dropdown-item" role="menuitem" tabindex="0" data-menu="settings">My Profile</span>
-            <a class="dropdown-item" role="menuitem" tabindex="0" href="/logout" data-menu="logout" id="logoutLink">Log out</a>
+            <a class="dropdown-item" role="menuitem" tabindex="0" href="/logout" data-menu="logout" id="logoutLink">Log
+                out</a>
         </div>
     </div>
 </div>
+@section('script')
+<script src="{{ asset('js/user.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        populateUserName();
+    });
+</script>
+@endsection
