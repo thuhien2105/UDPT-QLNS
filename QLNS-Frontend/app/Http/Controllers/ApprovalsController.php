61cea7a6b1d2630b28011f631ca3166bd7f20f6a
dthgiang <duongthanhgiang.0108@gmail.com>
2024-09-01 05:54:01 +0700

    Update Controller Approvals

--- QLNS-Frontend/app/Http/Controllers/ApprovalsController.php
+++ QLNS-Frontend/app/Http/Controllers/ApprovalsController.php
@@ -10,19 +10,22 @@ class ApprovalsController extends Controller
     use Notification;
     public function getCategoryPage()
     {
-    	return view('page.approvals.category.index');
+        return view('page.approvals.category.index');
     }
     public function getFormPage()
     {
-    	return view('page.approvals.form.index');
+        return view('page.approvals.form.index');
     }
     public function getListPage()
     {
-    	return view('page.approvals.list.index');
+        return view('page.approvals.list.index');
     }
     public function getAddPage()
     {
-    	return view('page.approvals.add.index');
+        return view('page.approvals.add.index');
     }
-
+    public function getRequestListPage()
+    {
+        return view('page.approvals.request_list.index');
+    }
 }
--- QLNS-Frontend/routes/web.php
+++ QLNS-Frontend/routes/web.php
@@ -23,46 +23,47 @@ use App\Http\Controllers\NotificationController;
 // });
 
 Route::middleware('checkRole:manager')->group(function () {
-    Route::get('/employees/add',[EmployeeController::class, 'getCreatePage'])->name('employees-add');
-    Route::get('/employees/form',[EmployeeController::class, 'getFormPage'])->name('employees-form');
-    Route::get('/employees',[EmployeeController::class, 'getListPage'])->name('employees-list');
+    Route::get('/employees/add', [EmployeeController::class, 'getCreatePage'])->name('employees-add');
+    Route::get('/employees/form', [EmployeeController::class, 'getFormPage'])->name('employees-form');
+    Route::get('/employees', [EmployeeController::class, 'getListPage'])->name('employees-list');
 
 
-    Route::get('/campaign/add',[CampaignController::class, 'getCreatePage'])->name('campaign-add');
-    Route::post('/campaign/add',[CampaignController::class, 'createCampaign'])->name('campaign-add');
+    Route::get('/campaign/add', [CampaignController::class, 'getCreatePage'])->name('campaign-add');
+    Route::post('/campaign/add', [CampaignController::class, 'createCampaign'])->name('campaign-add');
 });
-Route::get('/gifts/gifts/sub-item/form',[GiftsController::class, 'getSubItemPage'])->name('gifts-gifts-form');
-Route::get('/gifts/gifts/form',[GiftsController::class, 'getGiftPage'])->name('gifts-gifts-form');
-Route::get('/gifts/gifts',[GiftsController::class, 'getGiftsPage'])->name('gifts-gifts-list');
-Route::get('/gifts/brands/form',[GiftsController::class, 'getBrandPage'])->name('gifts-brands-form');
-Route::get('/gifts/brands',[GiftsController::class, 'getBrandsPage'])->name('gifts-brands-list');
-Route::get('/gifts',[GiftsController::class, 'getCategoryPage'])->name('gifts-category');
-Route::get('/gifts/manager',[GiftsController::class, 'getManagerPage'])->name('gifts-manager');
+Route::get('/gifts/gifts/sub-item/form', [GiftsController::class, 'getSubItemPage'])->name('gifts-gifts-form');
+Route::get('/gifts/gifts/form', [GiftsController::class, 'getGiftPage'])->name('gifts-gifts-form');
+Route::get('/gifts/gifts', [GiftsController::class, 'getGiftsPage'])->name('gifts-gifts-list');
+Route::get('/gifts/brands/form', [GiftsController::class, 'getBrandPage'])->name('gifts-brands-form');
+Route::get('/gifts/brands', [GiftsController::class, 'getBrandsPage'])->name('gifts-brands-list');
+Route::get('/gifts', [GiftsController::class, 'getCategoryPage'])->name('gifts-category');
+Route::get('/gifts/manager', [GiftsController::class, 'getManagerPage'])->name('gifts-manager');
 
-Route::post('/employees/add',[EmployeeController::class, 'createEmployee'])->name('employees-create');
-Route::put('/employees/edit',[EmployeeController::class, 'editEmployee'])->name('employees-edit');
-Route::get('/approvals',[ApprovalsController::class, 'getCategoryPage'])->name('approvals-category');
-Route::get('/approvals/form',[ApprovalsController::class, 'getFormPage'])->name('approvals-form');
-Route::get('/approvals/list',[ApprovalsController::class, 'getListPage'])->name('approvals-list');
-Route::get('/check-in-out',[EmployeeController::class, 'getCheckinoutPage'])->name('check-in-out');
-Route::get('/check-in-out/manager',[EmployeeController::class, 'getCheckinoutManagerPage'])->name('check-in-out-manager');
+Route::post('/employees/add', [EmployeeController::class, 'createEmployee'])->name('employees-create');
+Route::put('/employees/edit', [EmployeeController::class, 'editEmployee'])->name('employees-edit');
+Route::get('/approvals', [ApprovalsController::class, 'getCategoryPage'])->name('approvals-category');
+Route::get('/approvals/form', [ApprovalsController::class, 'getFormPage'])->name('approvals-form');
+Route::get('/approvals/list', [ApprovalsController::class, 'getListPage'])->name('approvals-list');
+Route::get('/check-in-out', [EmployeeController::class, 'getCheckinoutPage'])->name('check-in-out');
+Route::get('/check-in-out/manager', [EmployeeController::class, 'getCheckinoutManagerPage'])->name('check-in-out-manager');
 Route::post('/check-in', [EmployeeController::class, 'checkIn'])->name('check.in');
 Route::post('/check-out', [EmployeeController::class, 'checkOut'])->name('check.out');
-Route::get('/user',[UserController::class, 'getFormPage'])->name('user-form');
-Route::get('/campaign/form',[CampaignController::class, 'getFormPage'])->name('campaign-form');
-Route::get('/campaign',[CampaignController::class, 'getListPage'])->name('campaign-list');
+Route::get('/user', [UserController::class, 'getFormPage'])->name('user-form');
+Route::get('/campaign/form', [CampaignController::class, 'getFormPage'])->name('campaign-form');
+Route::get('/campaign', [CampaignController::class, 'getListPage'])->name('campaign-list');
 
 // Routes không cần role middleware
-Route::get('/',[PageController::class, 'getHomePage'])->name('trang-chu');
-Route::get('/login',[PageController::class, 'getLoginPage'])->name('login');
-Route::get('/signup',[PageController::class, 'getSignupPage'])->name('signup');
-Route::post('/login',[PageController::class, 'checkLogin'])->name('login.check');
-Route::post('/signup',[PageController::class, 'checkSignup'])->name('signup.check');
+Route::get('/', [PageController::class, 'getHomePage'])->name('trang-chu');
+Route::get('/login', [PageController::class, 'getLoginPage'])->name('login');
+Route::get('/signup', [PageController::class, 'getSignupPage'])->name('signup');
+Route::post('/login', [PageController::class, 'checkLogin'])->name('login.check');
+Route::post('/signup', [PageController::class, 'checkSignup'])->name('signup.check');
 
 // Route::get('/approvals', [ApprovalsController::class, 'index']);
 // Route::get('/approvals/{id}', [ApprovalsController::class, 'show']);
 Route::get('/approvals/add', [ApprovalsController::class, 'getAddPage']);
 Route::post('/approvals/add', [ApprovalsController::class]);
+Route::get('/approvals/request_list', [ApprovalsController::class, 'getRequestListPage'])->name('approvals-request-list');
 // Route::put('/approvals/{id}', [ApprovalsController::class, 'update']);
 // Route::delete('/approvals/{id}', [ApprovalsControllre::class, 'destroy']);
 
